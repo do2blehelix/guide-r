b4d3157248b7d6d1def36fcaae56437ee2c0dd8d
Ayan Banerjee <25700971+do2blehelix@users.noreply.github.com>
2019-08-01 15:16:06 +0530

    Update Clustering Example.R

--- clustering/Clustering Example.R
+++ clustering/Clustering Example.R
@@ -27,3 +27,31 @@ fviz_dist(res.dist,
 
 
 
+
+
+
+
+ibrary(factoextra)
+library(NbClust)
+
+df <- scale(USArrests)
+head(df)
+
+
+# Elbow method
+  fviz_nbclust(df, kmeans, method = "wss") +
+  geom_vline(xintercept = 4, linetype = 2)+
+  labs(subtitle = "Elbow method")
+
+# Silhouette method
+  fviz_nbclust(df, kmeans, method = "silhouette")+
+  labs(subtitle = "Silhouette method")
+
+
+# Gap statistic
+# nboot = 50 to keep the function speedy. 
+# recommended value: nboot= 500 for your analysis.
+# Use verbose = FALSE to hide computing progression.
+  set.seed(123)
+  fviz_nbclust(df, kmeans, nstart = 25,  method = "gap_stat", nboot = 50)+
+    labs(subtitle = "Gap statistic method")
